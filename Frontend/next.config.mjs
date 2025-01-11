0348337464c05e47d84704ffc2f63f4c8e9c3c34
Alfi <149055347+megaxusprime@users.noreply.github.com>
2025-01-11 23:51:52 +0700

    Delete Frontend directory

--- Frontend/next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        domains: ['i.pravatar.cc', 'github.com'], // Menambahkan github.com
-    },
-};
-
-export default nextConfig;
